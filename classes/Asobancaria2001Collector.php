e550d78d59e243218d03c44bff33de81ad2b6113
alucardthefish <sergiop18@gmail.com>
2021-03-29 21:29:28 -0500

    Add layout of classes that will read and load collection data from files

---
+++ classes/Asobancaria2001Collector.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\classes;
+
+class Asobancaria2001Collector implements ICollector {
+
+    public function getCollectionsArray($collectionFiles)
+    {
+        echo "Getting the collections array from Asobancaria2001 txt file";
+    }
+}
---
+++ classes/ExcellCollector.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\classes;
+
+class ExcellCollector implements ICollector {
+
+    public function getCollectionsArray($collectionFiles)
+    {
+        echo "Getting the collection array from excel file";
+    }
+}
