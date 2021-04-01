777355cfca313cdeec8a173aaf7ffbe227168c6a
alucardthefish <sergiop18@gmail.com>
2021-03-31 23:17:54 -0500

    Use recaudos script inside aso2001 collector class

--- classes/Asobancaria2001Collector.php
+++ classes/Asobancaria2001Collector.php
@@ -8,6 +8,8 @@ class Asobancaria2001Collector implements ICollector {
 
     public function getCollectionsArray($collectionFiles)
     {
+        $collectionArrayToApplyPayments = new Recaudos($collectionFiles);
+        $collectionArrayToApplyPayments->getAllData();
         return "<br>Getting the collections array from Asobancaria2001 txt file";
     }
 }
